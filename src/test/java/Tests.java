4bff3ec6334e57b6e338f7e9196a0756a0d0d3ac
hallbanaan <kersti.kalbus@gmail.com>
2017-09-24 11:07:01 +0300

    Test names
    To be continued, koeraga peab jalutama

---
+++ src/test/java/Tests.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+public class Tests {
+
+    @Test
+    public void invalidCoordinatesShouldReturnZero(){
+
+    }
+
+    @Test
+    public void testAPIConnection(){
+
+    }
+
+    @Test
+    public void 
+}
