c36998e75504e6d2e3a5467054b37fed65acfc7b
hallbanaan <kersti.kalbus@gmail.com>
2017-10-08 11:33:05 +0300

    added unitChange method

--- src/main/java/weatherRepo/WeatherRepo.java
+++ src/main/java/weatherRepo/WeatherRepo.java
@@ -33,13 +33,27 @@ public class WeatherRepo implements Weather{
         throw new UnsupportedOperationException("Coming soon");
     }
 
+    public static String changeWeatherUnit(String newUnit){
+        if (newUnit == "Metric" || newUnit == "Imperial" || newUnit == "Kelvin"){
+            units = newUnit;
+            return "Units sucessfully changed to: " + newUnit;
+        } else {
+            return "Unit change failed.";
+        }
+    }
+
     public static void setApiKey(String apiKey){
         WeatherRepo.apiKey = apiKey;
     }
+    public static String getApiKey(){return apiKey;}
+
     public static void setApiCallUrl(String apiCallUrl){
         WeatherRepo.apiCallUrl = apiCallUrl;
     }
+    public static String getApiCallUrl(){return apiCallUrl;}
+
     public static void setUnits(String units){
         WeatherRepo.units = units;
     }
+    public static String getUnits(){return units;}
 }
