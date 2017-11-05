878de33ad3b6a136425071a34f092fdb6f4bf590
hallbanaan <kersti.kalbus@gmail.com>
2017-11-05 10:34:07 +0200

    Fixed path, now reads input cityName from file

--- src/main/java/model/Files.java
+++ src/main/java/model/Files.java
@@ -1,21 +1,19 @@
 package model;
 
 import java.io.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Files {
 
     public static String readInputCityNameFromFile() {
-        File file = new File("C:\\Users\\Kersti\\Documents\\GitHub\\elutahenemine\\Failid");
+        File file = new File("C:\\Users\\Kersti\\Documents\\GitHub\\elutahenemine\\input.txt");
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
             StringBuffer fileContents = new StringBuffer();
             String line = br.readLine();
             fileContents.append(line);
-            line = br.readLine();
             br.close();
             String cityName = fileContents.toString();
+            System.out.println(cityName);
             return cityName;
         } catch (FileNotFoundException e){
             System.out.println(e.getMessage());
